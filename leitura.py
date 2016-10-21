df14de11685c7f9202b92fb02997eb2a1e688fb3
unvexp <unvexp@gmail.com>
2016-10-21 18:06:06 -0200

    fix

--- leitura.py
+++
@@ -1,7 +0,0 @@
-
-def ler_gramatica(caminho_arquivo):
-	with open(caminho_arquivo) as arquivo:
-	   for linha in arquivo:
-	   		itens = linha.split('->')
-	   		variavel_temp = itens[0]
-	   		cadeia_temp = itens[1]
