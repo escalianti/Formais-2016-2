e76515b5709b5575c1beddf037ac4cfb7a6728e7
unvexp <unvexp@gmail.com>
2016-10-21 16:39:07 -0200

    alguns comentários

--- main.py
+++ main.py
@@ -23,35 +23,36 @@ terminais = []
 terminais.append('A')
 terminais.append('B')
 terminais.append('a')
-producao = Producao(variavel, terminais)
+producao = Producao(variavel, terminais) #cria a producao
 
-lista_producoes.append(producao)
+lista_producoes.append(producao) # add a producao na lista
 
 variavel = 'S'
 terminais = []
 terminais.append('b')
-producao = Producao(variavel, terminais)
+producao = Producao(variavel, terminais) #cria a producao
 
-lista_producoes.append(producao)
+lista_producoes.append(producao) # add a producao na lista
 
 variavel = 'A'
 terminais = []
 terminais.append('a')
-producao = Producao(variavel, terminais)
+producao = Producao(variavel, terminais) #cria a producao
 
-lista_producoes.append(producao)
+lista_producoes.append(producao) # add a producao na lista
 
 variavel = 'B'
 terminais = []
 terminais.append('b')
-producao = Producao(variavel, terminais)
+producao = Producao(variavel, terminais) #cria a producao
 
 lista_producoes.append(producao)
 
 variavel = 'B'
 terminais = []
 terminais.append('V')
-producao = Producao(variavel, terminais)
+producao = Producao(variavel, terminais) # add a producao na lista
+
 
 
 
